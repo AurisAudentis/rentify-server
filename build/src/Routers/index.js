745a30a6c9e3c51b9823e47e9e63d03c037dd3be
mggeldho <maxiem.geldhof@ugent.be>
2019-03-16 21:56:51 +0100

    removing build

--- build/src/Routers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-exports.indexRouter = express.Router();
-/* GET home page. */
-exports.indexRouter.get('/', function (req, res) {
-    res.json({ "hello": "world" });
-});
