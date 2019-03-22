26af5ef81d4e81817a8bc31072447f06e495046a
AurisAudentis <maxiem@maxiemgeldhof.com>
2019-03-22 10:45:17 +0100

    adding refresh token func

--- test/AuthTest.py
+++ test/AuthTest.py
@@ -3,30 +3,36 @@ import requests
 oauthurl = "https://maxiemgeldhof.com"
 localurl = "http://localhost:4200"
 
+refresh = ""
+
 
 def fetchToken(mail, password):
+    global refresh
     r = requests.post(oauthurl + "/oauth/token", data={"email": mail, "pw":password})
-    print(r.json())
     token = r.json()["jwt"]
+    refresh = r.json()["refr"]
     return token
+
+def fetchTokenByRefr(mail, token):
+    r = requests.post(oauthurl + "/oauth/token", data={"email": mail, "token":token})
+    return r.json()["jwt"]
 
 def getHeader(token):
     return {"Authorization":"bearer " + token}
 
 def registerBackend(mail, password):
     r = requests.post(oauthurl + "/oauth/sso/register", data={"email": mail, "password": password})
-    print(r.json())
     return r.json()["uid"]
 
 
-email = "random16@rand.be"
+email = "random42@rand.be"
 password = "random_password"
 uid = registerBackend(email, password)
 
 r = requests.put(localurl + "/auth/register", 
     data={"mail": email, "oid": uid, "full_name":"Joris Landuyt", "phonenum":"0456789865"})
 
-print(r.text)
-
-r = requests.get(localurl + "/", headers=getHeader(fetchToken(email, password)))
+fetchToken(email, password)
+print(getHeader(fetchTokenByRefr(email, refresh))))
+r = requests.get(localurl + "/", headers=getHeader(fetchTokenByRefr(email, refresh)))
 print(r.text)
